94c4864617618ba58aafcb408a9af2d7a8d0291f
RyuJung <ryujung@163.com>
2023-04-16 14:55:24 +0800

    fix: fix the vue.config.js file problem and config meta info of project.

--- zhiqi-ui/src/views/login.vue
+++ zhiqi-ui/src/views/login.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="login">
     <!-- TODO login form html-->
-    <el-form :ref="loginForm" :model="loginForm" :rules="loginRules" label-width="100px" class="login-form">
-      <el-form-item label="用户名" prop="pass">
-        <el-input type="text" v-model="loginForm.password" autocomplete="off"></el-input>
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" label-width="100px" class="login-form">
+      <h3 class="title">知其后台管理系统</h3>
+      <el-form-item label="用户名" prop="validateUsername">
+        <el-input type="text" v-model="loginForm.username" autocomplete="off" placeholder="用户名"></el-input>
       </el-form-item>
 
-      <el-form-item label="密码" prop="checkPass">
-        <el-input type="password" v-model="loginForm.checkPass" autocomplete="off"></el-input>
-      </el-form-item>
-
-      <el-form-item>
-        <el-checkbox label="记住密码">记住密码</el-checkbox>
+      <el-form-item label="密码" prop="validatePassword">
+        <el-input type="password" v-model="loginForm.password" autocomplete="off" placeholder="密码"></el-input>
       </el-form-item>
 
       <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
+        <el-checkbox label="记住密码" v-model="loginForm.rememberMe">记住密码</el-checkbox>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button :loading="loading" type="primary" size="medium" style="width: 100%"
+          @click.native.prevent="handlerLogin">
+          <span v-if="!loading">登录</span>
+          <span v-else>登 录 中...</span>
+        </el-button>
+        <div>
+          <!-- TODO register router link -->
+        </div>
       </el-form-item>
     </el-form>
-
+    <div class="el-login-foooter">
+      <span>Copyright © 2023-2023 ZhiQi All Rights Reserved.</span>
+    </div>
   </div>
 </template>
 
@@ -47,14 +56,14 @@ export default {
         uuid: ''
       },
       loginRules: {
-        username: [
+        validateUsername: [
           { require: true, type: 'string', trigger: 'blur', message: '请输入您的账号' }
         ],
-        checkPass: [
+        validatePassword: [
           { require: true, trigger: 'blur', message: '请输入您的密码' }
         ],
-        code: [
-          { require: true, trigger: 'blur', message: '请输入验证码' }
+        validateCode: [
+          { require: true, trigger: 'change', message: '请输入验证码' }
         ]
       }
     }
@@ -69,14 +78,14 @@ export default {
     }
   },
   created() {
-    getKaptchaCode();
-    getUserInfoFromCookie();
+    getKaptchaCode()
+    getUserInfoFromCookie()
   },
   methods: {
     getKaptchaCode() {
-      getCodeImg().then(res => {
+      getCodeImg().then((res) => {
         // TODO captcha request and show
-      });
+      })
     },
     getUserInfoFromCookie() {
       const username = Cookies.get('username')
@@ -84,36 +93,42 @@ export default {
       const rememberMe = Cookies.get('remeberMe')
       this.loginForm = {
         username: username === undefined ? this.loginForm.username : username,
-        password: password === undefined ? this.loginForm.password : decrypt(password),
+        password:
+          password === undefined ? this.loginForm.password : decrypt(password),
         rememberMe: rememberMe === undefined ? false : Boolean(rememberMe)
       }
     },
     handlerLogin() {
-      this.$refs.loginForm.validate(valid => {
+      this.$refs.loginForm.validate((valid) => {
         if (valid) {
-          this.loading = true;
+          this.loading = true
           if (this.loginForm.rememberMe) {
             Cookies.set('username', this.loginForm.username, { expire: 30 })
-            Cookies.set('password', encrypt(this.loginForm.password), { expire: 30 })
+            Cookies.set('password', encrypt(this.loginForm.password), {
+              expire: 30
+            })
             Cookies.set('rememberMe', this.loginForm.rememberMe, { expire: 30 })
           } else {
             Cookies.remove('username')
             Cookies.remove('password')
             Cookies.remove('rememberMe')
           }
-          this.$store.dispatch('Login', this.loginForm).then(() => {
-            // TODO request login api and handle the response
-          }).catch(() => {
-            this.loading = false
-            // TODO handle error login response
-          })
+          this.$store
+            .dispatch('Login', this.loginForm)
+            .then(() => {
+              // TODO request login api and handle the response
+            })
+            .catch(() => {
+              this.loading = false
+              // TODO handle error login response
+            })
           login(username, password, code, uuid)
         } else {
-          console.log('error submit!!');
-          return false;
+          console.log('error submit!!')
+          return false
         }
-      });
+      })
     }
-  },
+  }
 }
 </script>
